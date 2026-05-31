0b7143f0d41387d9c11989e34d42dacfa1a3ba56
Guilherme006 <guilherme_lazari@outlook.com>
2026-05-31 01:28:46 -0300

    feat: add processed event query port

---
+++ src/main/java/com/gubee/stockreconciliation/domain/port/in/GetProcessedStockEventUseCase.java
@@ -0,0 +1,10 @@
+package com.gubee.stockreconciliation.domain.port.in;
+
+import com.gubee.stockreconciliation.domain.model.ProcessedStockEvent;
+
+import java.util.Optional;
+
+public interface GetProcessedStockEventUseCase {
+
+    Optional<ProcessedStockEvent> getProcessedEvent(String eventId);
+}
